b194bf5774c787aa005529adbb23d69ddf6ece06
web3nomad.eth <me@web3nomad.eth>
2023-12-04 00:56:44 +0800

    callback on image generated

--- api-server/src/routes/image.rs
+++ api-server/src/routes/image.rs
@@ -1,16 +1,11 @@
 use serde_json::json;
 use std::env;
-// use reqwest;
 use std::sync::Arc;
-use tokio::sync::{
-    // watch,
-    mpsc,
-};
+use tokio::sync::mpsc;
 use axum::{
     routing::post,
     response::{Json, IntoResponse, Response},
     Router,
-    // body::Body,
     http::StatusCode,
 };
 
@@ -26,21 +21,22 @@ impl IntoResponse for BadRequest {
     }
 }
 
-async fn handler(body: String, tx: Arc<mpsc::Sender<String>>) -> Result<Json<serde_json::Value>, BadRequest> {
+async fn handler_generate_image(
+    body: String,
+    tx: Arc<mpsc::Sender<String>>
+) -> Result<Json<serde_json::Value>, BadRequest> {
     if let Ok(()) = tx.try_send(body) {
         let res_json = json!({
             "taskId": "fed8d585-4ca7-4cda-a41e-16bb9a7c93c3"
         });
         return Ok(Json(res_json));
     } else {
-        // let res_json = json!({
-        //     "success": false
-        // });
         return Err(BadRequest { message: "Queue is full".to_string() });
     }
 }
 
-async fn request_webui(prompt: &str, timestamp: &str) {
+async fn request_webui(params: &serde_json::Value) -> Vec<String> {
+    let prompt = params["prompt"].as_str().unwrap();
     let webui_origin = env::var("SD_WEBUI_TEST_ORIGIN").unwrap();
     let api_auth = Some(sdwebuiapi::OpenApiV1Auth {
         username: env::var("SD_WEBUI_TEST_AUTH_USERNAME").unwrap(),
@@ -48,16 +44,43 @@ async fn request_webui(prompt: &str, timestamp: &str) {
     });
     let mut payload = sdwebuiapi::TextToImagePayload {
         prompt: prompt.to_string(),
+        batch_size: 4,
         ..Default::default()
     };
     payload.set_base_model("DreamShaper_6_BakedVae.safetensors [b76cc78ad9]");
     let client = sdwebuiapi::Client::new(&webui_origin, api_auth);
     let response = client.txt2img(payload).await;
-    let raw_b64_str = &response.images[0];
-    // TODO: remove this debug code in production
-    let output_image = data_encoding::BASE64.decode(raw_b64_str.as_bytes()).unwrap();
-    std::fs::create_dir_all("test-client/output").unwrap();
-    std::fs::write(format!("test-client/output/{}.png", timestamp), output_image).unwrap();
+    let images = response.images.iter().map(|raw_b64_str| {
+        raw_b64_str.to_string()
+    }).collect::<Vec<String>>();
+    return images;
+}
+
+async fn handler_generate_image_callback(
+    task_id: &str,
+    params: &serde_json::Value,
+    images: &Vec<String>,
+    callback_url: &str,
+) {
+    let mut images_payload: Vec<String> = vec![];
+    for (i, raw_b64_str) in images.iter().enumerate() {
+        // let raw_b64_str = &response.images[i];
+        // TODO: remove this debug code in production
+        let output_image = data_encoding::BASE64.decode(raw_b64_str.as_bytes()).unwrap();
+        std::fs::create_dir_all("test-client/output").unwrap();
+        let filename = format!("test-client/output/{}-{}.png", task_id, i);
+        std::fs::write(&filename, output_image).unwrap();
+        images_payload.push(filename);
+    }
+    let payload = json!({
+        "taskId": task_id,
+        "params": params,
+        "result": {
+            "images": images_payload
+        }
+    });
+    let client = reqwest::Client::new();
+    client.post(callback_url).json(&payload).send().await.unwrap();
 }
 
 pub fn get_routes() -> Router {
@@ -67,23 +90,23 @@ pub fn get_routes() -> Router {
 
     tokio::spawn(async move {
         while let Some(body) = rx.recv().await {
-            // let n = rand::random::<u8>() % 5 + 1;
-            // tokio::time::sleep(tokio::time::Duration::from_secs(n.into())).await;
-            // let timestamp = chrono::Local::now().format("%Y-%m-%d %H:%M:%S");
-            // println!("{} Done! {}", timestamp, body);
             let body_json = serde_json::from_str::<serde_json::Value>(&body).unwrap();
-            let prompt = body_json["params"]["prompt"].as_str().unwrap();
-            let timestamp = body_json["timestamp"].as_str().unwrap();
-            println!("Start! {}", timestamp);
-            request_webui(prompt, timestamp).await;
-            println!("End! {}", timestamp);
+            let callback_url = body_json["resultCallbackUrl"].as_str().unwrap();
+            let params = body_json["params"].clone();
+            let task_id = body_json["timestamp"].as_str().unwrap();
+            println!("Start! {}", task_id);
+            let images = request_webui(&params).await;
+            handler_generate_image_callback(
+                task_id, &params, &images, callback_url
+            ).await;
+            println!("End! {}", task_id);
         }
     });
 
     let router: Router = Router::new()
         .route("/api/yum/generate/image", post({
             let tx = Arc::clone(&tx);
-            |body: String| handler(body, tx)
+            |body: String| handler_generate_image(body, tx)
         }));
     return router;
 }
