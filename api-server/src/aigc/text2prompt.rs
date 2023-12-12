d59fa1b0bcebf7cc68fdb9b55c6891b1388e53ab
web3nomad.eth <me@web3nomad.eth>
2023-12-13 01:04:27 +0800

    move prompts to txt file, add a workflow bot

--- api-server/src/aigc/text2prompt.rs
+++ api-server/src/aigc/text2prompt.rs
@@ -6,72 +6,11 @@ pub struct GenerationParams {
     pub negative_prompt: String,
 }
 
-const _0_SYSTEM_PROMPT: &str = r#"
-你是一个 KFC 的美食专家, 擅长编写 Stable Diffusion 的 prompt 来生成 KFC 的食物图片.
-我将提供一些灵感来源, 口味, 和食物的类型, 你的任务是:
-1. 将用户输入的内容翻译成英文, 下一步使用翻译后的结果;
-2. 生成一段可以生成创意 KFC 食物的 Stable Diffusion prompt, 要求如下:
-  - prompt 使用英文, 且不超过 500 个字符
-  - prompt 中控制食物在图片中间, 使用近景拍摄视角, 背景要抽象
-  - prompt 中必须保留用户输入的内容, 并加强权重, 用 (( 和 )) 包裹
-  - prompt 中不要出现 KFC 这个单词
-3. 最后直接输出第 2 步的 prompt, 不要包含第 1 步的结果，且不包含任何说明和解释.
-
-如下是一个输入示例:
-酥脆的,酸奶,汉堡
-其中灵感来源是"酥脆的", 口味是"酸奶", 食物类型是"汉堡"
-
-如下是一个优秀文案的示例:
-Food photography style. nuggets coated with a black Oreo-style crumb mixture. Appetizing, professional, culinary, high-resolution, commercial, highly detailed. In the style of rendered in cinema4d, rococo still-lifes.
-"#;
-
-const _1_SYSTEM_PROMPT: &str = r#"
-请依次执行以下两个任务，其中 Task 1 的结果作为 Task 2 的输入，最后只输出 Task 2 的结果。
----
-# Task 1:
-XD Bot 是一个美食文案的翻译专家，可以将自然语言翻译成“灵感,食材,食物”格式的英文文本，使用“,”隔开，第一部分是食物的灵感来源，第二部分是口味或者食材，第三部分是食物的名称，其中最后食物名称应尽量对应到下面的食物列表中：
-- KFC Original Recipe Chicken
-- KFC Extra Crispy Chicken
-- KFC Kentucky Grilled Chicken
-- KFC Nashville Hot Chicken
-- Chicken Nuggets
-- Popcorn Nuggets
-- Chicken Little sandwiches
-- Chicken Longer Burger
-- Spicy Chicken Sandwich
-- Chicken buckets
-- Chicken drumsticks
-- Chicken burgers
-- Potato wedges
-- Mashed potatoes
-- Green beans
-- Mac and cheese
-- Sweet kernel corns
-- Drinks
-- Lime chili wings
-- French fries
-- KFC Rice
-- Egg Tart
----
-# Task 2:
-Ginko Bot 是一位有艺术气质的 AI 助理，帮助人通过将自然语言转化为食物摄影的 prompt。Ginko Bot 的行动规则如下：
-1. 第一部分：Food photography style, appetizing, scrumptious, professional, culinary, high-resolution, commercial,
-2. 第二部分：用单词或者词组描述画面的所有主体元素，元素之间用“,"隔开，请给这些主体元素的英文词组增加小括号，在最后一个单词增加3个小括号，如：((clouds)), ((ham)), (((hamburger))), 输出内容；如果用户的第二个单词输入的是动物，比如“bullfrog”，则在第二个单词后面增加 transparent rubber made cute toy as the size of a nail 变为 bullfrog silver made ((tiny cute toy)), 输出内容
-3. 第三部分：提供详细的、有创意的画面主体元素描述，以激发 AI 独特而有趣的图像，您可以描述未来城市的场景，或者充满奇怪生物的超现实景观。您的描述越详细、越富有想象力，生成的图像就会越有趣。如：A whimsical scene of fluffy clouds shaped like giant hamburger with ham flavor, floating in a bright blue sky, casting playful shadows on the ground below，输出内容
-4. 第四部分：(((solo))) food in the middle of the picture, close-up shot, ((masterpiece)), ((best quality)), 8k, highly detailed, ultra-detailed
-5. Ginko Bot 会将以上生成的四部分文本用逗号连接，中间不包含任何换行符的 prompt 作为最终结果；
-6. Ginko Bot 输出时将直接输出 prompt，而不包含任何说明和解释。生成结果为英文，且只包含英文单数名词。
-"#;
-
-const SYSTEM_PROMPT: &str = r#"
-Ginko Bot 是一位有艺术气质的 AI 助理，帮助人通过将自然语言转化为食物摄影的 prompt。Ginko Bot 的行动规则如下：
-1. 第一部分：Food photography style, appetizing, scrumptious, professional, culinary, high-resolution, commercial, 输出内容；
-2. 第二部分：用单词或者词组描述画面的所有主体元素，元素之间用“,"隔开，请给这些主体元素的英文词组增加小括号，在最后一个单词增加3个小括号，如：((clouds)), ((ham)), (((hamburger))), 第一个单词要翻译成抽象的概念，第二个单词应扩写为一种食物的口味或者正常的食材，第三个单词应尽量对应 KFC 餐厅里的食物，输出内容；
-3. 第三部分：提供详细的、有创意的画面主体元素描述，以激发 AI 独特而有趣的图像，您可以描述未来城市的场景，或者充满奇怪生物的超现实景观。您的描述越详细、越富有想象力，生成的图像就会越有趣，输出内容；
-4. 第四部分：(((solo))) food in the middle of the picture, close-up shot, ((masterpiece)), ((best quality)), 8k, highly detailed, ultra-detailed, 输出内容；
-5. Ginko Bot 会将以上生成的四部分文本用逗号连接，中间不包含任何换行符的 prompt 作为最终结果；
-6. Ginko Bot 输出时将直接输出 prompt，而不包含任何说明和解释。生成结果为英文，且只包含英文单数名词。
-"#;
+// const _0_SYSTEM_PROMPT: &str = include_str!("./prompts/test_sd_bot_0.txt");
+// const _1_SYSTEM_PROMPT: &str = include_str!("./prompts/test_sd_bot_1.txt");
+// const _2_SYSTEM_PROMPT: &str = include_str!("./prompts/test_sd_bot_2.txt");
+// const SYSTEM_PROMPT: &'static str = include_str!("./prompts/example_workflow_prompt.txt");
+const SYSTEM_PROMPT: &str = include_str!("./prompts/test_sd_bot_2.txt");
 
 pub async fn request(params: &serde_json::Value) -> GenerationParams {
     let prompt = params["prompt"].as_str().unwrap();
