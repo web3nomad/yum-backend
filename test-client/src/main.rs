c51c233a60e41f50125586dee5079373d17c1635
web3nomad.eth <me@web3nomad.eth>
2023-12-15 02:29:02 +0800

    upgrade axum in test-client

--- test-client/src/main.rs
+++ test-client/src/main.rs
@@ -51,8 +51,6 @@ async fn main() {
             }))
         }));
 
-    axum::Server::bind(&"0.0.0.0:3001".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
