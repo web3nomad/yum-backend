5d32d2a17048cc5b65d6bc335ec9018272ee5c98
web3nomad.eth <me@web3nomad.eth>
2023-12-26 01:00:12 +0800

    modify text generate prompt

--- api-server/src/routes/text.rs
+++ api-server/src/routes/text.rs
@@ -7,23 +7,34 @@ use axum::{
 };
 
 const SYSTEM_PROMPT: &str = r#"
-你是一个 KFC 的美食专家，擅长撰写创意的 KFC 食物的名字。
-我将提供一些灵感来源，口味，和食物的类型，你的任务是：
-  - 生成一个创意的 KFC 食物的名字
-  - 直接输出名字，不要包含任何标点符号或者表情
-  - 长度在 4 到 8 个汉字之间
-如下是一个优秀文案的示例：
-  - 灯影云面包
-  - 绿野仙踪鸡腿堡
-  - 三潭印月脆皮鸡堡
+你是一个 KFC 的美食专家，擅长撰写创意的<美食帖子分享标题>。
+
+## 用户输入的信息格式为：
+  - xxx, xxx, xxx (用户输入的<灵感来源>、<灵感配料>、<美食类别>，以逗号分隔)
+
+## 你的任务是：
+  - 根据用户输入的<灵感来源>、<灵感配料>、<美食类别>，撰写一个美食帖子分享标题。如下是一个优秀文案的示例：
+    - 灯影云面包
+    - 绿野仙踪鸡腿堡
+    - 三潭印月脆皮鸡堡
+
+## 需要注意的要求：
+  - 使用中文输出
+  - 输出结果不要超过 8 个汉字
+  - 输出结果只能包含<美食帖子分享标题>，不要加任何解释说明，也不要添加任何标点符号或者表情
+
+---
+
+开始！请根据用户输入的<灵感来源>、<灵感配料>、<美食类别>，仅输出 <美食帖子分享标题> ！！！
 "#;
 
 async fn handler(body: String) -> Result<Json<serde_json::Value>, impl IntoResponse> {
     let json_body: serde_json::Value = serde_json::from_str(&body).unwrap();
     let params = &json_body["params"];
     let prompt = json_body["params"]["prompt"].as_str().unwrap();
+    tracing::info!("Text {} started", prompt);
     let message = match crate::aigc::openai::request(
-        "gpt-35-turbo", &SYSTEM_PROMPT, prompt, 1.0, false
+        "gpt-35-turbo", &SYSTEM_PROMPT, prompt, 0.8, false
     ).await {
         Ok(v) => v,
         Err(e) => {
@@ -31,6 +42,7 @@ async fn handler(body: String) -> Result<Json<serde_json::Value>, impl IntoRespo
             return Err((StatusCode::INTERNAL_SERVER_ERROR, "Server Error").into_response());
         }
     };
+    tracing::info!("Text {} end {}", prompt, message);
     let res_json = json!({
         "params": params,
         "result": {
