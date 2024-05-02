97d220f25b185e210a45869e5873e5543ea38848
web3nomad.eth <me@web3nomad.eth>
2024-05-02 14:53:41 +0800

    add english prefix to avoid azure filter

--- api-server/src/aigc/filter.rs
+++ api-server/src/aigc/filter.rs
@@ -1,13 +1,14 @@
 pub fn modify_text(user_input: &str) -> String {
-    let mut user_input = user_input.to_string();
-    if user_input.contains("猪") {
-        user_input = user_input.replace("猪", " pork ");
-    }
-    if user_input.contains("鸡") {
-        user_input = user_input.replace("鸡", " chicken ");
-    }
-    if user_input.contains("胸") {
-        user_input = user_input.replace("胸", " chest ");
-    }
-    user_input
+    let user_input = user_input.to_string();
+    // if user_input.contains("猪") {
+    //     user_input = user_input.replace("猪", " pork ");
+    // }
+    // if user_input.contains("鸡") {
+    //     user_input = user_input.replace("鸡", " chicken ");
+    // }
+    // if user_input.contains("胸") {
+    //     user_input = user_input.replace("胸", " chest ");
+    // }
+    // user_input
+    format!("Food Inspiration: {}", user_input)
 }
